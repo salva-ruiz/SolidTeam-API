536759b1f5d1b2cc2e5b8daeb7635fecc3af5f34
Salvador Ruiz <info@ok-computer.es>
2019-06-30 01:39:46 +0200

    Added Users REST routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({name: 'salva'})
-})
+router
+
+  /* GET users listing */
+  .get('/', function(req, res, next) {
+    knex('users')
+      .select(['id', 'user_name', 'full_name', 'email_address', 'time_zone'])
+      .then(users => {
+        res.json(users)
+      })
+  })
+
+  /* GET a user */
+  .get('/:id(\\d+)', function(req, res, next) {
+    knex('users')
+      .select(['id', 'user_name', 'full_name', 'email_address', 'time_zone'])
+      .where('id', req.params['id'])
+      .then(user => {
+        if (user.length)
+          res.json(user)
+        else
+          res.send(404)
+      })
+  })
+
+  /* DELETE a user */
+  .delete('/:id(\\d+)', function(req, res, next) {
+    knex('users')
+      .where('id', req.params['id'])
+      .delete()
+      .then(count => {
+        if (count)
+          res.json({message: 'User deleted'})
+        else
+          res.send(404)
+      })
+  })
 
 module.exports = router
